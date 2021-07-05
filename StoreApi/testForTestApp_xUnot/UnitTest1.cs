97cf689eb63ae5edc83cfd7f97a4c41abd6ceebd
ilya_ermaykin <ilya_ermaykin@mail.ru>
2021-07-05 15:39:59 +0300

    #8 - testTask_ch;  - Разбор unit-тестов;  - Подключено тестовове приложение, реализованы 2 варианта тестов, проверка строк;

---
+++ StoreApi/testForTestApp_xUnot/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace testForTestApp_xUnot
+{
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+
+        [Fact]
+        public void Test1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                testApp.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.Equal(Expected, result);
+            }
+
+        }
+    }
+}
